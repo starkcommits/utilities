ea729050a3304e401b12d364d52b646f1bd8cc7e
starkcommits <tinkal.kumar@xettle.net>
2025-02-08 10:12:24 +0000

    added doctypes and workspace

---
+++ utility/utility/doctype/channel_partner/channel_partner.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, xFer India100x pvt. ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Channel Partner", {
+// 	refresh(frm) {
+
+// 	},
+// });
